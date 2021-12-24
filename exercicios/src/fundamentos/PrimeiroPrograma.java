6ccd08e49c5a973170f567f86a4bb3538478a894
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-12-23 23:43:13 -0300

    Comentários

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,23 @@
 package fundamentos;
 
+/**
+ * 
+ * @author Kleber
+ * @since 2021
+ * @see github.com/kleber086
+ */
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
+		
+		// Comentário de uma linha
 		System.out.println("Hello World!");
+		
+		/*
+		 * Comentário de multiplas linhas
+		 */
 		System.out.println("Primeiro Programa #2");
+		
+		System.out.println("FIM"); // Comentário válido.
 	}
 }
