9647f4472a62ed19a5f068d6318a892d6f45516b
Kleber Nascimento <klebernascimento@outlook.com.br>
2021-12-29 23:33:41 -0300

    Objeto vs Primitivo

---
+++ exercicios/src/fundamentos/PrimitivoVsObjeto.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class PrimitivoVsObjeto {
+	public static void main(String[] args) {
+		
+		String nome = new String("Fulano");
+		System.out.println(nome);
+		
+		// Wrappers
+		int idade = 15;
+		System.out.println(idade);
+	}
+}
