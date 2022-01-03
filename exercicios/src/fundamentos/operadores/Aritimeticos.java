6849a52dec44dc82ed332b30a5a1f4205be6c782
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-01-03 20:53:12 -0300

    Operadores Lógicos #02

---
+++ exercicios/src/fundamentos/operadores/Aritimeticos.java
@@ -0,0 +1,29 @@
+package fundamentos.operadores;
+
+public class Aritimeticos {
+
+	public static void main(String[] args) {
+		System.out.println(2 + 2);
+		
+		var a = 23.2;
+		double b = 1.2;
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a / b);
+		System.out.println(a * b);
+		
+		int c = 10;
+		int d = 5;
+		System.out.println(c + d);
+		System.out.println(c - d);
+		System.out.println(c / d);
+		System.out.println(c * d);
+		System.out.println(c / (double) d);
+		
+		System.out.println(c % d);
+		System.out.println(a % b);
+		
+		System.out.println(a - b / c * d);
+	}
+}
