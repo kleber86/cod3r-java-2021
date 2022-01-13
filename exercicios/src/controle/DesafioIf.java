09ca62c58e7508cd6e51a2383688ed4afc36eee8
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-01-13 19:05:26 -0300

    Controle: Desafio if

---
+++ exercicios/src/controle/DesafioIf.java
@@ -0,0 +1,12 @@
+package controle;
+
+public class DesafioIf {
+	public static void main(String[] args) {
+		double nota = 4;
+		
+		if(nota >= 9.8);{
+			System.out.println("Aprovado");
+			System.out.println("Quadro de Honra");
+		}
+	}
+}
