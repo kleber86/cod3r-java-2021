4ef183d68ccab923e63e164694e76cf781947185
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-01-19 20:50:24 -0300

    Desafio: For

---
+++ exercicios/src/controle/DesafioFor.java
@@ -0,0 +1,9 @@
+package controle;
+
+public class DesafioFor {
+	public static void main(String[] args) {
+		for(String x = "#";  !x.equals("######"); x+= "#") {
+			System.out.println(x);
+		}
+	}
+}
