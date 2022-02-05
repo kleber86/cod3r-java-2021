904b49846558732f156a1c9cb97f01a24f449010
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-02-04 21:31:28 -0300

    Enum

---
+++ exercicios/src/oo/composicao/heranca/Direcao.java
@@ -0,0 +1,5 @@
+package oo.composicao.heranca;
+
+public enum Direcao {
+	NORTE, SUL, LESTE, OESTE;
+}
---
+++ exercicios/src/oo/composicao/heranca/Jogador.java
@@ -0,0 +1,25 @@
+package oo.composicao.heranca;
+
+public class Jogador {
+
+	int x;
+	int y;
+	
+	boolean andar(Direcao direcao) {
+		switch(direcao) {
+		case NORTE:
+			y--;
+			break;
+		case SUL:
+			y++;
+			break;
+		case LESTE:
+			x++;
+			break;
+		case OESTE:
+			x--;
+			break;
+		}
+		return true;
+	}
+}
