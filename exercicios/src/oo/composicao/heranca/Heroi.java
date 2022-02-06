ec9a4df79f2816ebe5d07609fe5f23dcc15f9bf3
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-02-05 23:58:58 -0300

    Herança #03

--- exercicios/src/oo/composicao/heranca/Heroi.java
+++ exercicios/src/oo/composicao/heranca/Heroi.java
@@ -2,7 +2,11 @@ package oo.composicao.heranca;
 
 public class Heroi extends Jogador{
 	
-boolean atacar(Jogador oponente) {
+	Heroi(int x, int y){
+		super(x, y);
+	}
+	
+	boolean atacar(Jogador oponente) {
 		
 	boolean ataque1 = super.atacar(oponente);
 	boolean ataque2 = super.atacar(oponente);
