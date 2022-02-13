0191e3cd45c6d5259f3c6aa6ad51168cbf7b1aa3
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-02-13 12:32:57 -0300

    Reduce #04

---
+++ exercicios/src/streams/Reduce3.java
@@ -0,0 +1,32 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Reduce3 {
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno("Ana", 7.3);
+		Aluno a2 = new Aluno("Gui", 5.1);
+		Aluno a3 = new Aluno("Bia", 9.0);
+		Aluno a4 = new Aluno("Dan", 10);
+		
+		List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4);
+		
+		Predicate<Aluno> aprovado = a -> a.nota >= 7;
+		Function<Aluno, Double> apenasNotas = a -> a.nota;
+		BiFunction<Media, Double, Media> calcularMedia = (media, nota) -> media.adicionar(nota);
+		BinaryOperator<Media> combinarMedia = (m1, m2) -> Media.combinar(m1, m2);
+		
+		Media media = alunos.parallelStream()
+		.filter(aprovado)
+		.map(apenasNotas)
+		.reduce(new Media(), calcularMedia, combinarMedia);
+		
+		System.out.println("Calculo da media da turma " + media.getValor());
+		
+	}
+}
