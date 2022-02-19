13e1b5700447d1f2a36fcc9f53b5d00a10af2bf1
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-02-19 14:10:18 -0300

    Explorando os Tipos de JOINs

--- banco-relacional/inserirPrefeitos.sql
+++ banco-relacional/inserirPrefeitos.sql
@@ -1,8 +1,13 @@
-SELECT * from cidades;
+SELECT * from prefeitos;
 
 INSERT INTO prefeitos
   (nome, cidade_id)
 VALUES
   ('Rodrigo Neves', 7),
-  ('Raquel Lyra', 8)
-  ('Zenaldo Coutinho', null)
+  ('Raquel Lyra', 8),
+  ('Zenaldo Coutinho', null);
+
+INSERT INTO prefeitos
+  (nome, cidade_id)
+VALUES
+  ('RAfael Creca', null);
