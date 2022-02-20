e5bd9a1c38fe432186c190a18ccb351187f91d0a
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-02-20 10:07:11 -0300

    Obter Usuários

--- exercicios-jpa/src/main/java/teste/basico/obterUsuarios.java
+++ exercicios-jpa/src/main/java/teste/basico/obterUsuarios.java
@@ -1,12 +1,15 @@
 package teste.basico;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import modelo.basico.Usuario;
 
-public class NovoUsuario {
+public class obterUsuarios {
 
 	public static void main(String[] args) {
 		EntityManagerFactory emf = 
@@ -14,13 +17,17 @@ public class NovoUsuario {
 		EntityManager em = emf.createEntityManager();
 		
 		
-		Usuario novoUsuario = new Usuario("Pipen", "pipen@email.com");
+		String jpql = "SELECT u FROM Usuario u";
+		TypedQuery<Usuario> query = em.createQuery(jpql, Usuario.class);
+		query.setMaxResults(5);
 		
-		em.getTransaction().begin();
-		em.persist(novoUsuario);
-		em.getTransaction().commit();
+		List<Usuario> usuarios = query.getResultList();
 		
-		System.out.println("Usuario com ID: " + novoUsuario.getId());
+		for(Usuario usuario : usuarios) {
+			System.out.println(
+					"ID: " + usuario.getId()+
+					" E-mail: " + usuario.getEmail());
+		}
 		
 		em.close();
 		emf.close();
