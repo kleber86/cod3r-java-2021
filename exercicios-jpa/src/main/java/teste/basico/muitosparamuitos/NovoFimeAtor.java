c5e0ed8919ca6fa8f5e6e7997deb6869284202c9
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-02-26 19:38:10 -0300

    Muitos Pra Muitos #02

---
+++ exercicios-jpa/src/main/java/teste/basico/muitosparamuitos/NovoFimeAtor.java
@@ -0,0 +1,22 @@
+package teste.basico.muitosparamuitos;
+
+import infra.DAO;
+import modelo.muitosparamuitos.Ator;
+import modelo.muitosparamuitos.Filme;
+
+public class NovoFimeAtor {
+
+	public static void main(String[] args) {
+		Filme filmeA = new Filme("Star Wars Ep 4", 8.7);
+		Filme filmeB = new Filme("O Figitivo", 8.9);
+		
+		Ator atorA = new Ator("Harrison Ford");
+		Ator atrizB = new Ator("Carrie Fisher");
+		
+		filmeA.adicionarAtor(atorA);
+		filmeA.adicionarAtor(atrizB);
+		
+		DAO<Filme> dao = new DAO<Filme>();
+		dao.incluirAtomico(filmeA);
+	}
+}
