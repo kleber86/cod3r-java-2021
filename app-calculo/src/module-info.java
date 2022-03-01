6778f0cf9ba1c2c7430e3fc70d45c3dc66c4e0bb
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-03-01 14:21:31 -0300

    Módulos e Reflection

--- app-calculo/src/module-info.java
+++ app-calculo/src/module-info.java
@@ -4,4 +4,6 @@ module app.calculo {
 	exports br.com.cod3r.app.calculo;
 	
 	exports br.com.cod3r.app.calculo.interno to app.financeiro;
+	
+	opens br.com.cod3r.app.calculo to app.financeiro, app.logging;
 	}
--- app.financeiro/src/br/com/cod3r/app/financeiro/Teste.java
+++ app.financeiro/src/br/com/cod3r/app/financeiro/Teste.java
@@ -1,5 +1,7 @@
 package br.com.cod3r.app.financeiro;
 
+import java.lang.reflect.Field;
+
 import br.com.cod3r.app.calculo.Calculadora;
 import br.com.cod3r.app.calculo.interno.OperacoesAritmeticas;
 
@@ -11,5 +13,17 @@ public class Teste {
 		
 		OperacoesAritmeticas op = new OperacoesAritmeticas();
 		System.out.println(op.soma(4, 5, 7));
+		
+		try {
+			Field fieldID = Calculadora.class.getDeclaredFields()[0];
+			fieldID.setAccessible(true);
+			fieldID.set(calc, "def");
+			fieldID.setAccessible(false);
+			
+			System.out.println(calc.getId());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
