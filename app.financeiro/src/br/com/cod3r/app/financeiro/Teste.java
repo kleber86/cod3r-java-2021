f4ab36fee8692155f707fec20e9dd6f7dd86f10c
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-03-01 13:31:42 -0300

    Exports To

--- app.financeiro/src/br/com/cod3r/app/financeiro/Teste.java
+++ app.financeiro/src/br/com/cod3r/app/financeiro/Teste.java
@@ -1,11 +1,15 @@
 package br.com.cod3r.app.financeiro;
 
 import br.com.cod3r.app.calculo.Calculadora;
+import br.com.cod3r.app.calculo.interno.OperacoesAritmeticas;
 
 public class Teste {
 
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
 		System.out.println(calc.soma(1.1, 2.3));
+		
+		OperacoesAritmeticas op = new OperacoesAritmeticas();
+		System.out.println(op.soma(4, 5, 7));
 	}
 }
