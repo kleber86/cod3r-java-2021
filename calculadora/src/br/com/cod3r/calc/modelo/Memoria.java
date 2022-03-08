da75ff8afd6b0f421784507adb056aaeed468667
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-03-07 21:38:31 -0300

    Lógica da Calculadora #02

--- calculadora/src/br/com/cod3r/calc/modelo/Memoria.java
+++ calculadora/src/br/com/cod3r/calc/modelo/Memoria.java
@@ -1,8 +1,14 @@
 package br.com.cod3r.calc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Memoria {
 
 	private static final Memoria instancia = new Memoria();
+	
+	private final List<MemoriaObservador> observadores = 
+			new ArrayList<>();
 	
 	private String textoAtual = "";
 	
@@ -14,10 +20,23 @@ public class Memoria {
 		return instancia;
 	}
 
+	public void adiconarObservador(MemoriaObservador o) {
+		observadores.add(o);
+	}
+	
 	public String getTextoAtual() {
 		return textoAtual.isEmpty() ? "0" : textoAtual;
 	}
 
-	
+	public void processarComando(String valor) {
+		
+		if("AC".equals(valor)) {
+			textoAtual = "";
+		}else {
+			textoAtual += valor;
+		}
+		
+		observadores.forEach( o -> o.valorAlterado(getTextoAtual()));
+	}
 	
 }
