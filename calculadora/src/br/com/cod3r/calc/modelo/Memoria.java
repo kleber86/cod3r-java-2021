5f91d4d4a42498f6e12f72b3c0bc5fa810363a21
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-03-07 22:17:33 -0300

    Lógica da Calculadora #05

--- calculadora/src/br/com/cod3r/calc/modelo/Memoria.java
+++ calculadora/src/br/com/cod3r/calc/modelo/Memoria.java
@@ -49,9 +49,42 @@ public class Memoria {
 				tipoComando == TipoComando.VIRGULA){
 			textoAtual = substituir ? texto : textoAtual + texto;
 			substituir = false;
+		}else {
+			substituir = true;
+			textoAtual = obterResultadoOperacao();
+			textoBuffer = textoAtual;
+			ultimaOperacao = tipoComando;
 		}
 
 		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+	}
+
+	private String obterResultadoOperacao() {
+		if(ultimaOperacao == null 
+				|| ultimaOperacao == TipoComando.IGUAL) {
+			return textoAtual;
+		}
+		
+		double numeroBuffer = 
+				Double.parseDouble(textoBuffer.replace(",", "."));
+		double numeroAtual = 
+				Double.parseDouble(textoAtual.replace(",", "."));
+		
+		double resultado = 0;
+		
+		if(ultimaOperacao == TipoComando.SOMA) {
+			resultado = numeroBuffer + numeroAtual;
+		} else if(ultimaOperacao == TipoComando.SUB) {
+			resultado = numeroBuffer - numeroAtual;
+		} else if(ultimaOperacao == TipoComando.MULT) {
+			resultado = numeroBuffer * numeroAtual;
+		} else if(ultimaOperacao == TipoComando.DIV) {
+			resultado = numeroBuffer / numeroAtual;
+		}
+		
+		String texto = Double.toString(resultado).replace(".", ",");
+		boolean inteiro = texto.endsWith(",0");
+		return inteiro ? texto.replace(",0", "") : texto;
 	}
 
 	private TipoComando detectarTipoComando(String texto) {
