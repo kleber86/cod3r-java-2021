590abbcd52a9b75097ac1fb83964255c1855183b
Kleber Nascimento <klebernascimento@outlook.com.br>
2022-03-07 21:15:15 -0300

    Componente Botao

--- calculadora/src/br/com/cod3r/calc/visao/Botao.java
+++ calculadora/src/br/com/cod3r/calc/visao/Botao.java
@@ -1,5 +1,19 @@
 package br.com.cod3r.calc.visao;
 
-public class Botao {
+import java.awt.Color;
+import java.awt.Font;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+
+public class Botao extends JButton{
+
+	public Botao(String texto, Color cor) {
+		setText(texto);
+		setOpaque(true);
+		setBackground(cor);
+		setFont(new Font("courier", Font.PLAIN, 25));
+		setForeground(Color.WHITE);
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	}
 }
